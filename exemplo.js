819dc5dc378c1254d114d848de407b130d3db398
GUILHERME VERAS DA ROCHA <57481277842@imaccommand014.local>
2025-11-18 10:46:55 -0300

    Exercicios01

---
+++ exemplo.js
@@ -0,0 +1,51 @@
+// //Alert - apresenta uma menssagem na tela
+// window.alert("Ola Turma do borges")
+// //Confirm - pergunta com ok/cancela (Retorna verdadeiro ou falso)
+// window.confirm("O Jonas vai casar!")
+// //Prompt - pega o texto da tela
+// window.prompt("Qual o seu nome?")
+
+// // Comentario em linha
+// /*
+// comentario em texto
+// */
+
+// console.log("Multimidia")
+
+// let nome = prompt ("Informe o seu nome: ")
+
+// console.log("bem vindo! " + nome)
+
+// // let numero1 = Number.parseInt(prompt("Digite o primeiro número: "))
+// // let numero2 = Number.parseInt(prompt("Digite o segundo número: "))
+
+// // let resultado = numero1 + numero2
+
+// let numero1 = Number.parseFloat(prompt("Digite o primeiro número: "))
+// let numero2 = Number.parseFloat(prompt("Digite o segundo número: "))
+
+// let resultado = numero1 + numero2
+
+// console.log(resultado)
+
+// let idade = window.prompt("Qual sua idade?")
+
+// if (idade >=18){
+//     console.log("Maior de Idade! Da-lheee")
+// }else{
+//     console.log("Você é menor de idade!")
+// }
+
+// switch (Idade) {
+//     case "18":
+//         console.log("Maior de Idade!")
+//         break;
+
+//     case "27":
+//         console.log("Esta perto dos 30!")
+//         break
+
+//     default:
+//         console.log("nada a dizer")
+//         break;
+// }
---
+++ exercicios.js
@@ -0,0 +1,8 @@
+let login = prompt ("informe seu nome: Admin")
+let senha = prompt ("informe sua senha: senha123")
+
+if (login == "Admin" && senha == "senha123") {
+    console.log ("Login bem sucedido!")
+} else {
+    console.log ("Você não tem permissão para esse acesso")
+}
