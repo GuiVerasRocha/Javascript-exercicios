cf956f98166f5cfec2fd7a70ce7ca9872e03cd29
GuiVerasRocha <guilhermevrocha45@gmail.com>
2025-11-25 14:40:39 -0300

    Exercicio07

---
+++ Exercicio07.js
@@ -0,0 +1,10 @@
+let numero = parseInt(prompt("Digite um numero para a contagem regressiva"))
+
+console.log("Contagem regressiva está começando em " + numero)
+
+while(numero >= 0){
+    console.log(numero)
+    numero--
+}
+
+console.log("contagem regressiva finalizada")
